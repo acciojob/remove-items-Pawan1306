7bef8165e1f872a3f11e0ca71941b9049b655c9a
Pawan1306 <141865155+Pawan1306@users.noreply.github.com>
2023-10-25 18:36:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-//your JS code here. If required.
+ document.getElementById("removeButton").addEventListener("click", function () {
+        var select = document.getElementById("colorSelect");
+        var selectedOption = select.options[select.selectedIndex];
+        if (selectedOption) {
+          select.removeChild(selectedOption);
+        }
+      });
